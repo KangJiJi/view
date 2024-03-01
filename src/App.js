a1a917401c3eda30f750aa4b571463f7cdab6958
KangJiJi <kjhmd9726@gmail.com>
2024-03-01 23:45:39 +0900

    deploy - 2024-03-01 23:45:39

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 
-import trashCan from 'asset/image/trashCan.png';
-
 function App() {
   return (
     <div className="App">
       <div className="TranshCanLogo">
-        <img src={trashCan} alt="Trash can" />
+        <div>안녕 진!</div>
       </div>
     </div>
   );
