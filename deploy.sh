e98f67afa89f3137415169051ed049131ec589d6
KangJiJi <kjhmd9726@gmail.com>
2024-03-02 01:15:21 +0900

    deploy - 2024-03-02 01:15:21

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,15 @@ get_current_time() {
     date +"%Y-%m-%d %H:%M:%S"
 }
 
-convert_to_kst() {
-    TZ=":Asia/Seoul" date
-}
-
 cd ./../
 rsync -av --delete --exclude=node_modules/ --exclude=build/ --exclude=.git/ "./$source_dir/" "./$target_dir/"
 
 current_time=$(get_current_time)
-current_time_kst=$(convert_to_kst)
 
-commit_message="deploy - $current_time_kst"
+commit_message="deploy - $current_time"
 
 cd "$target_dir" || exit
 
 git add .
 git commit -m "$commit_message"
+gi tpush origin main
